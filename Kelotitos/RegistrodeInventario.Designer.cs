efc280ecc95f3c4c685972e6cf6d6ec390e72dd0
VaniaVitela <viajero34922@innovaccion.mx>
2020-10-04 14:36:07 -0500

    tengo 5 errores:(

--- Kelotitos/RegistrodeInventario.Designer.cs
+++ Kelotitos/RegistrodeInventario.Designer.cs
@@ -32,12 +32,13 @@
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.label3 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.cmb = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -75,28 +76,6 @@
             this.textBox2.Size = new System.Drawing.Size(392, 22);
             this.textBox2.TabIndex = 3;
             // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(390, 240);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(65, 21);
-            this.radioButton1.TabIndex = 4;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Botes";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(529, 240);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(71, 21);
-            this.radioButton2.TabIndex = 5;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Bolsas";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
             // numericUpDown1
             // 
             this.numericUpDown1.Location = new System.Drawing.Point(245, 239);
@@ -113,16 +92,6 @@
             this.label3.TabIndex = 7;
             this.label3.Text = "Cantidad";
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(290, 331);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(90, 34);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "Añadir";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // button3
             // 
             this.button3.Location = new System.Drawing.Point(460, 331);
@@ -133,21 +102,63 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(382, 239);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Categoría:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // cmb
+            // 
+            this.cmb.FormattingEnabled = true;
+            this.cmb.Items.AddRange(new object[] {
+            "Botes ",
+            "Bolsas "});
+            this.cmb.Location = new System.Drawing.Point(472, 236);
+            this.cmb.Name = "cmb";
+            this.cmb.Size = new System.Drawing.Size(144, 24);
+            this.cmb.TabIndex = 13;
+            this.cmb.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(245, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 34);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Aceptar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmb);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "RegistrodeInventario";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -163,11 +174,12 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ComboBox cmb;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Kelotitos/RegistrodeInventario.cs
+++ Kelotitos/RegistrodeInventario.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Kelotitos.ModelsDB;
+using Kelotitos.MySql;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,24 +15,21 @@ namespace Kelotitos
 {
     public partial class Form1 : Form
     {
+        private int resultado;
+        private object name_textbox;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Producto Agregado");
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -39,12 +39,69 @@ namespace Kelotitos
         private void button3_Click(object sender, EventArgs e)
         {
             ResetText();
-            MessageBox.Show("Se eliminara la información de los campos llenados", "¿Está segur@ de eliminar la información?", MessageBoxButtons.OK);
+            MessageBox.Show("Se eliminará la información de los campos llenados", "¿Está segur@ de eliminar la información?", MessageBoxButtons.OK);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            providerAccount newProviderAccount = NewMethod();
+            newProviderAccount.name_prov = name_textbox.Text.Trim();
+            newProviderAccount.name_prov = textBox2.Text.Trim();
+            newProviderAccount.cant_prov = numericUpDown1.Value.ToString();
+            newProviderAccount.cat_prov = cmb.Items.AddRange();
+            if (string.IsNullOrEmpty(name_textbox.Text) || string.IsNullOrEmpty(textBox2.Text) || string.IsNullOrEmpty(numericUpDown1) || string.IsNullOrEmpty(cmb))
+            {
+                MessageBox.Show("Los campos requieren de una respuesta", "RegistroInventario", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MySqlConnection conexion = Connection.GetConnection();
+                MySqlCommand newProvider = new MySqlCommand();
+                newProvider.CommandText = "SELECT * FROM provider WHERE name_prov";
+                newProvider.Parameters.AddWithValue("@newProviderAccount.name_prov", newProviderAccount.name_prov);
+                newProvider.Parameters.AddWithValue("@newProviderAccount.name_prov", newProviderAccount.name_prov);
+                newProvider.Parameters.AddWithValue("@newProvideraAccount.cant_prov", newProviderAccount.cant_prov);
+                newProvider.Parameters.AddWithValue("@newProviderAccount.cat_prov", newProviderAccount.cat_prov);
+                newProvider.Connection = conexion;
+                MySqlDataReader leer = newProvider.ExecuteReader();
+                if (leer.Read())
+                {
+                    MessageBox.Show("El producto ya existe", "RegistroInventario", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    conexion.Close();
+                }
+                else
+                {
+                    int v = RegisterNewProvider.agregar(newProviderAccount);
+                    int resultado = v;
+                }
 
+                if (resultado > 0)
+                {
+                    MessageBox.Show("Producto Registrado con Éxito!", "RegistroInventario", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Elegir ToElegir = new Elegir();
+                    this.Hide();
+                    ToElegir.Show();
+                }
+                else
+                {
+                    MessageBox.Show("No se puede guardar el Producto", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private static providerAccount NewMethod()
+        {
+            return newProviderAccount();
+        }
+
+        private static providerAccount newProviderAccount()
+        {
+            throw new NotImplementedException();
         }
     }
 }
