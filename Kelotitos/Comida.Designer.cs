29a9fb3d8c3ad5191513d885c2593db07f8d1e67
Gisela L <asoraleci@hotmail.com>
2020-10-05 13:12:02 -0500

    3 cambio prueba:B

--- Kelotitos/Comida.Designer.cs
+++ Kelotitos/Comida.Designer.cs
@@ -82,7 +82,7 @@
             this.lbhora.AutoSize = true;
             this.lbhora.BackColor = System.Drawing.Color.Transparent;
             this.lbhora.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbhora.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.lbhora.ForeColor = System.Drawing.Color.DarkOrange;
             this.lbhora.Location = new System.Drawing.Point(756, 40);
             this.lbhora.Name = "lbhora";
             this.lbhora.Size = new System.Drawing.Size(73, 24);
@@ -355,8 +355,9 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Bookman Old Style", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label1.ForeColor = System.Drawing.Color.DarkOrange;
             this.label1.Location = new System.Drawing.Point(67, 193);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(195, 56);
